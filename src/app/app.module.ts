249efa8c02f016de2833630112406b191e95be18
aarontravass <aaron.travass@hotmail.com>
2022-11-13 02:21:25 +0000

    added files. angular still broken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsComponent } from './charts/charts.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes =[
+    { path: '', component: AppComponent },
+    { path: 'bot', component: BotpageComponent },
+    { path: 'charts', component: ChartsComponent },
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BotpageComponent,
+        ChartsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes, {
+            useHash: false, 
+            scrollPositionRestoration: 'enabled',
+            enableTracing: true
+        });
+        
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/botpage/botpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botpage',
+  templateUrl: './botpage.component.html',
+  styleUrls: ['./botpage.component.css']
+})
+export class BotpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
